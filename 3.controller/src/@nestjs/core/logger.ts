c657b8ce828559038451a70ab87151cc65647fed
GoldenZqqq <1361001127@qq.com>
2025-01-08 10:32:19 +0800

    chore: refactor import paths in TypeScript files for improved module resolution; enhance logger with time difference tracking for log entries.

--- 3.controller/src/@nestjs/core/logger.ts
+++ 3.controller/src/@nestjs/core/logger.ts
@@ -1,17 +1,21 @@
 import clc from "cli-color"
 export class Logger {
+  private static lastLogTime = Date.now()
   // 定义了一个用来打印日志的工具方法
   static log(message: string, context: string = "") {
     // 获取当前时间戳
     const timestamp = new Date().toLocaleString()
     // 获取当前的进程ID
     const pid = process.pid
+    const currentTime = Date.now()
+    const timeDiff = currentTime - this.lastLogTime
     console.log(
       `[${clc.red("HZQNB:")} ${clc.green("Nest")}] ${clc.green(
         pid.toString()
       )}  - ${clc.yellow(timestamp)}     ${clc.green("LOG")} [${clc.yellow(
         context
-      )}] ${clc.green(message)}`
+      )}] ${clc.green(message)} ${clc.green(timeDiff.toString())}ms`
     )
+    this.lastLogTime = currentTime
   }
 }
